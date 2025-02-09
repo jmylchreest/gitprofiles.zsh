8144b0dd020ed6048fb708b7f9ec5eedd0b34d92
John Mylchreest <jmylchreest@technauts.co.uk>
2025-02-09 11:11:32 +0000

    Add initial support for multiple paths

--- gitprofiles.plugin.zsh
+++ gitprofiles.plugin.zsh
@@ -1,6 +1,7 @@
 # Copyright (c) Bruno Sales <me@baliestri.dev>. Licensed under the MIT License.
 # See the LICENSE file in the project root for full license information.
 
+# vim: set ts=4 sw=4 tw=0 et :
 #!/usr/bin/env zsh
 
 function __gitprofiles_hook() {
@@ -9,8 +10,8 @@ function __gitprofiles_hook() {
     return 1
   fi
 
-  local -A profile_path_map=()
-  local -A profile_cfg_map=()
+  typeset -A profile_paths_map
+  typeset -A profile_cfg_map
 
   ## Get the path to the profile file
   zstyle -s ":empresslabs:git:profile" path profile_filepath
@@ -29,28 +30,31 @@ function __gitprofiles_hook() {
     return 1
   fi
 
+  # Function to parse paths into an array
+  function __parse_paths() {
+    local raw_paths="$1"
+    echo ${(s:,:)raw_paths}  # Split by comma
+  }
+
   ## Iterate over all profiles to get the name, email, signingkey and path
   for profile in ${profiles}; do
-    local -A profile_value_map=()
+    typeset -A profile_value_map
 
     while read -r key value; do
       case "${key}" in
-        name)
-          profile_value_map[name]="${value}"
+        name|email|signingkey)
+          profile_value_map[${key}]="${value}"
           ;;
-        email)
-          profile_value_map[email]="${value}"
-          ;;
-        signingkey)
-          profile_value_map[signingkey]="${value}"
-          ;;
-        path)
-          profile_value_map[path]="${value}"
+        path|paths)
+          profile_value_map[paths]="${value}"
           ;;
       esac
     done < <(awk -F ' = ' '/^\[profile/{p=0} /^\[profile "[^"]*'"${profile}"'"/{p=1} p {gsub(/"/, "", $2); print $1,$2}' ${profile_filepath})
 
-    profile_path_map[${profile}]="${profile_value_map[path]}"
+    # Parse paths
+    if [[ -n "${profile_value_map[paths]}" ]]; then
+      profile_paths_map[${profile}]="$(__parse_paths "${profile_value_map[paths]}")"
+    fi
 
     profile_cfg_map[${profile}.name]="${profile_value_map[name]}"
     profile_cfg_map[${profile}.email]="${profile_value_map[email]}"
@@ -60,31 +64,47 @@ function __gitprofiles_hook() {
     fi
   done
 
-  ## Get the current directory
-  local -A current=()
-  current[dir]=$(pwd)
+  # Get current directory
+  local current_dir=$(pwd)
+  local matched_profile="default"
 
-  ## Check if the current directory is in one of the profiles paths
-  for profile in ${(k)profile_path_map}; do
-    if [[ "${current[dir]}" =~ "${profile_path_map[${profile}]}" ]]; then
-      local current[profile]="${profile}"
-      break
-    fi
+  # Check if current directory matches any profile paths
+  for profile in ${(k)profile_paths_map}; do
+    local paths=(${=profile_paths_map[${profile}]})  # Convert to array
+
+    for path_pattern in $paths; do
+      # Remove leading/trailing whitespace
+      path_pattern="${path_pattern## }"
+      path_pattern="${path_pattern%% }"
+
+      if [[ "${current_dir}" =~ "${path_pattern}" ]]; then
+        matched_profile="${profile}"
+        break 2
+      fi
+    done
   done
 
-  ## If the current directory is not in any profile path, use the default profile
-  if [[ -z "${current[profile]}" ]]; then
-    local current[profile]="default"
-  fi
-
   ## Set the current profile name and email
-  git config --global user.name "${profile_cfg_map[${current[profile]}.name]}"
-  git config --global user.email "${profile_cfg_map[${current[profile]}.email]}"
+  git config --global user.name "${profile_cfg_map[${matched_profile}.name]}"
+  git config --global user.email "${profile_cfg_map[${matched_profile}.email]}"
 
   ## Set the current profile signingkey if it exists
-  if [[ -n "${profile_cfg_map[${current[profile]}.signingkey]}" ]]; then
-    git config --global user.signingkey "${profile_cfg_map[${current[profile]}.signingkey]}"
+  if [[ -n "${profile_cfg_map[${matched_profile}.signingkey]}" ]]; then
+    git config --global user.signingkey "${profile_cfg_map[${matched_profile}.signingkey]}"
+  fi
+
+  # Print debug information if DEBUG is set
+  if [[ -n "${DEBUG}" ]]; then
+    echo "Current directory: ${current_dir}"
+    echo "Matched profile: ${matched_profile}"
+    echo "Using configuration:"
+    echo "  name: ${profile_cfg_map[${matched_profile}.name]}"
+    echo "  email: ${profile_cfg_map[${matched_profile}.email]}"
+    if [[ -n "${profile_cfg_map[${matched_profile}.signingkey]}" ]]; then
+      echo "  signingkey: ${profile_cfg_map[${matched_profile}.signingkey]}"
+    fi
   fi
 }
+
 
 add-zsh-hook chpwd __gitprofiles_hook
